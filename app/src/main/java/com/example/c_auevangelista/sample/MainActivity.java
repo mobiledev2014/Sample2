bc93d916e2ac89d90a61d0493c26e2814da33cad
AnjeeloEvans <citmobiledev01@gmail.com>
2018-07-03 15:48:54 +0800

    Initial Commit

--- app/src/main/java/com/example/c_auevangelista/sample/MainActivity.java
+++ app/src/main/java/com/example/c_auevangelista/sample/MainActivity.java
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(context, 2);
         mRecyclerView.setLayoutManager(layoutManager);
 
+        //Testing1
 
     }
 
