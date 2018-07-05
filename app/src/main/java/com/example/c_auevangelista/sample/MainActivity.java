72038c1254fe558dee6f3ba7feb0c5e808dca02f
bert <c_jhcanuto@unilab.com.ph>
2018-07-05 13:10:20 +0800

    Bert - activity - description2

--- app/src/main/java/com/example/c_auevangelista/sample/MainActivity.java
+++ app/src/main/java/com/example/c_auevangelista/sample/MainActivity.java
@@ -28,7 +28,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+//bert
     public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
 
     private RecyclerView mRecyclerView;
